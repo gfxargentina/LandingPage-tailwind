a813b6f21c2a02ca8449dc8a05d15badc8ed66ac
Luis Chilo <gfxargentina@gmail.com>
2021-10-04 19:51:34 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa';
+
+
+export const Navbar = () => {
+    return (
+        <header>
+            <div className="logo" >
+
+            </div>
+            <nav>
+                <ul className="li">
+                    <li>Inicio</li>
+                    <li>Blog</li>
+                    <li>Contacto</li>
+                </ul>
+                
+            </nav>
+        </header>
+    )
+}
